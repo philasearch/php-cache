574f2a5809334b8484e4ed269e30366dd3c9fa10
Kujevic <kujevic@philasearch.com>
2015-07-13 09:32:56 +0200

    Fixed wrong use of Join function in CommandException.php

--- lib/Philasearch/Cache/Providers/Redis/Exceptions/CommandException.php
+++ lib/Philasearch/Cache/Providers/Redis/Exceptions/CommandException.php
@@ -6,7 +6,7 @@ class CommandException extends \Exception
 {
     public function __construct($function, $args, $code = 0, Exception $previous = null)
     {
-        $message = "Command '{$function}' with arguments " . join( $args, ", " ) . " failed!";
+        $message = "Command '{$function}' with arguments " . join( ", ", $args ) . " failed!";
 
         parent::__construct($message, $code, $previous);
     }
